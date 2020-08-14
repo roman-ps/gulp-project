ffade521a9d5cb1635d4957d3a9598da75e3619c
Pozharov Roman <ru.roman.ps@gmail.com>
2020-08-14 16:05:15 +0400

    Добавил папку source

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
-/* 
+/*
   Author - Pozharov Roman
   email - ru.roman.ps@gmail.com
 */
-    
+
 
 'use strict';
 
@@ -16,18 +16,18 @@ var gulp = require('gulp'),
     autoprefixer = require('gulp-autoprefixer'),
     imagemin = require('gulp-imagemin'),
     del = require('del'),
-    runsequence = require('run-sequence'); // плагин для последовательного выполнения задач 
-    
+    runsequence = require('run-sequence'); // плагин для последовательного выполнения задач
+
 
 gulp.task('styles', function() { // стили, префиксер, минификация
-  gulp.src('sass/style.scss')
+  gulp.src('source/sass/style.scss')
   .pipe(plumber())
   .pipe(sass())
   .pipe(autoprefixer({ browsers: ['last 4 versions'] }))
-  .pipe(gulp.dest('css'))
+  .pipe(gulp.dest('source/css'))
   .pipe(minify())
   .pipe(rename('style.min.css'))
-  .pipe(gulp.dest('css'))
+  .pipe(gulp.dest('source/css'))
   .pipe(reload({stream: true}));
 });
 
@@ -43,7 +43,7 @@ gulp.task('webserver', function(){  // запуск live-сервера
 // Сборка проекта
 
 gulp.task('css', function() {
-  gulp.src('sass/style.scss')
+  gulp.src('source/sass/style.scss')
   .pipe(plumber())
   .pipe(sass())
   .pipe(autoprefixer({ browsers: ['last 4 versions'] }))
@@ -54,23 +54,23 @@ gulp.task('css', function() {
 })
 
 gulp.task('html', function() {
-  gulp.src('*.html')
+  gulp.src('source/*.html')
   .pipe(gulp.dest('build'));
 })
 
 gulp.task('img', function() {
-  gulp.src('img/*')
+  gulp.src('source/img/*')
   .pipe(imagemin())
   .pipe(gulp.dest('build/img'));
 })
 
 gulp.task('fonts', function() {
-  gulp.src('fonts/*')
+  gulp.src('source/fonts/*')
   .pipe(gulp.dest('build/fonts'));
 })
 
 gulp.task('js', function() {
-  gulp.src('js/main.js')
+  gulp.src('source/js/main.js')
   .pipe(gulp.dest('build/js'));
 })
 
@@ -78,12 +78,12 @@ gulp.task('clean', function() {
   del('build');
 })
 
-gulp.task ('build', function(callback) { // запуск всех задач сборки 
+gulp.task ('build', function(callback) { // запуск всех задач сборки
   runsequence('clean', 'html', 'css', 'js', 'fonts', 'img', callback)
 })
 
 gulp.task ('watch', ['webserver', 'styles'], function() { // отслеживание изменения в файлах и перезагрузка
-  gulp.watch('sass/**/*.scss', ['styles']);
-  gulp.watch('*.html', server.reload);
-  gulp.watch('js/*.js', server.reload);
+  gulp.watch('source/sass/**/*.scss', ['styles']);
+  gulp.watch('source/*.html', server.reload);
+  gulp.watch('source/js/*.js', server.reload);
 });
